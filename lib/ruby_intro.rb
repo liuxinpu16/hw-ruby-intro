09153a1d93d37f007bf84d6451ce30fbf1048857
EC2 Default User <ec2-user@ip-172-31-43-161.us-east-2.compute.internal>
2020-01-31 03:04:33 +0000

    hw done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,92 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  len=arr.length
+  if len==0
+    return 0
+  elsif len==1
+    return arr[0]
+  else  
+    arrs = arr.sort
+    sum2 = arrs[-1]+arrs[-2]
+    return sum2
+  end
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  a=Hash.new(0)
+  b=arr.length
+  if b<2
+    return false
+  end
+  for i in 0..(b-1)
+    if a.has_key?(n-arr[i])
+      return true  
+    else 
+      a[arr[i]]=i
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  if s=~/^[a-zA-Z]/
+    if s=~/^[auoieAUIOE]/
+      return false
+    else
+      return true
+    end
+  else
+    return false
+  end
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  if (s=="")
+    return false
+  elsif (s=~/[^01]/) == 0
+    return false
+  end
+ 
+  if s =~ /00$/
+    return true
+  elsif s=='0'
+    return true
+  else
+    return false
+  end
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn,price)
+    if price <= 0
+      raise ArgumentError.new('Wrong Price') 
+    end
+    
+    if isbn.empty?
+      raise ArgumentError.new('Wrong ISBN')
+    end
+    
+    @isbn = isbn
+    @price = price
+    
+  end
+
+  attr_accessor :isbn, :price
+  
+  def price_as_string
+    '$%.2f' % @price
+  end
 end
